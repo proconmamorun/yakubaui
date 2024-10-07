96f47bd7d7d432cbe6c322706ccb94feff09caac
new-umechan <ryu.shamoji@gmail.com>
2024-10-07 19:47:35 +0900

    sideBarをnewsBelt対応

--- src/app/components/sideBar.tsx
+++ src/app/components/sideBar.tsx
@@ -3,7 +3,7 @@ import './sideBarDesign.css';
 
 // chatGPTなのでわからん
 interface SideBarProps {
-    setMode: (mode: 'home' | 'map' | 'SafeCheck') => void; // モード切り替え用の関数を受け取る
+    setMode: (mode: 'home' | 'map' | 'SafeCheck' | 'newsBelt') => void; // モード切り替え用の関数を受け取る
     setArea: (area: string) => void; 
 }
 
@@ -29,7 +29,7 @@ const SideBar: React.FC<SideBarProps> = ({ setMode, setArea }) => {
 		<button onClick={() => {setMode('SafeCheck'); setArea('阿野')}}>阿野</button>
 		<button onClick={() => {setMode('SafeCheck'); setArea('鬼籠野')}}>鬼籠野</button>
 
-		<button>帯表示</button>
+		<button onClick={() => {setMode('newsBelt'); setArea('newsBelt')}}>帯表示</button>
 		<button>危険を特定</button>
 		</div>
     </div>
