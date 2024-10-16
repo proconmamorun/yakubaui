358d847c90c583c71bf441bad30f1aad6ed73c2c
new-umechan <ryu.shamoji@gmail.com>
2024-10-16 17:23:58 +0900

    ホーム画面の、帯の部分を作りました。

--- src/app/components/homes/home.tsx
+++ src/app/components/homes/home.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import './homeDesign.css';  // 相対パスでCSSファイルをインポート
+import { Obi } from '../news/Obi';
 
 const Home: React.FC = () => {
     return (
 		<div>
 			<div className="home-container">
-				<div className={"green-box"}>
-					<div className={"alert"}>土砂崩れに警戒してください</div>
-					<div className={"alert"}>土砂崩れに警戒してください</div>
-				</div>
+				<Obi />
 				<div className={"map-box"}>
 				<p className="mapText">ここに地図テキスト</p>
 				</div>
--- src/app/components/news/Obi.tsx
+++ src/app/components/news/Obi.tsx
@@ -64,7 +64,7 @@ export function Obi() {
     return (
 		<div>
 			<div className="obiContainer">
-				<h2 className="mokuzi">最終確認</h2>
+				<h2 className="mokuzi">現在の帯</h2>
 				<div className= "obiPreview">
 					<div className="textSlider" ref={sliderRef}>
 						<img src="/images/obi-arrow.png" alt="矢印" className="arrowIcon" />
